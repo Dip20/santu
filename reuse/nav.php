c889fda7402f65b0da3852781b2c46277a80bb33
dip sarkar <info.dip.sarkar@gmail.com>
2022-01-03 03:12:03 +0530

    git init

---
+++ reuse/nav.php
@@ -0,0 +1,19 @@
+<header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a class=" font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <span class="ml-3 text-xl brand_name">Santu Sarkar</span>
+      </a>
+      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <a href="###" class="mx-5 hover:text-gray-900 text-gray-900 ">Home</a>
+        <a href="###" class="mx-5 hover:text-gray-900">About</a>
+        <a href="###" class="mx-5 hover:text-gray-900">Contact</a>
+        <a href="###" class="mx-5 hover:text-gray-900">Blogs</a>
+        <a href="###" class="mr-5 hover:text-gray-900">Business Enquery</a>
+      </nav>
+      <a href="tel:+918637554692" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 mx-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"><i class="fa fa-phone text-gray-500"></i> &nbsp; Give a Call    
+      </a>
+      
+      <a href="tel:+918637554692" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 mx-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"><i class="fa fa-briefcase text-gray-500"></i> &nbsp;  Hire Me
+      </a>
+    </div>
+  </header>
